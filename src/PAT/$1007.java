0041d1ad3009f0470cc65486b3beb92a31e777d5
VincentLaf <zsjperiod@foxmail.com>
2018-02-03 15:20:27 +0800

    甲级1007最大子序和不知为什么只对了一个测试用例...

--- src/PAT/$1007.java
+++ src/PAT/$1007.java
@@ -1,6 +1,8 @@
+package PAT;
+
 import java.util.Scanner;
 
-public class Main {
+public class $1007 {
 
     public static int minIndex;
     public static int maxIndex;
@@ -17,7 +19,7 @@ public class Main {
         if (hasPositive) {
             System.out.println(sum + " " + minIndex + " " + maxIndex);
         } else {
-            System.out.println(sum + " 0 " + (arr.length - 1));
+            System.out.println(sum + " 0 " + arr.length);
         }
     }
 
@@ -29,16 +31,21 @@ public class Main {
         //local
         int thisSum = 0;
         int thisMinIndex = 0;
+        int thisMaxIndex = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > 0) hasPositive = true;
             thisSum += arr[i];
+            thisMaxIndex = i;
             if (thisSum > maxSum) {
                 maxSum = thisSum;
-                maxIndex = i;
+                maxIndex = thisMaxIndex;
                 minIndex = thisMinIndex;
             } else if (thisSum < 0) {
                 thisSum = 0;
-                thisMinIndex = i + 1;
+                if (i != arr.length - 1) {
+                    thisMinIndex = i + 1;
+                    thisMaxIndex = i + 1;
+                }
             }
         }
         return maxSum;
