4742da3ce468858585c47cec461947babc88a36b
VincentLaf <zsjperiod@foxmail.com>
2018-02-07 21:04:41 +0800

    1058. A+B in Hogwarts

--- src/PAT/$1058.java
+++ src/PAT/$1058.java
@@ -1,8 +1,10 @@
+package PAT;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class $1058 {
 
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
