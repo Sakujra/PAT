ea096b11128b40ef77cb49972eb9e4155072e909
VincentLaf <zsjperiod@foxmail.com>
2018-02-07 19:56:11 +0800

    1042. Shuffling Machine

--- src/PAT/$1041.java
+++ src/PAT/$1041.java
@@ -3,9 +3,6 @@ package PAT;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class $1041 {
 
@@ -13,35 +10,29 @@ public class $1041 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
             String[] arrStr = reader.readLine().split(" ");
-            int count = Integer.parseInt(arrStr[0]);
-            Map<Integer, Integer> map = new LinkedHashMap<>();
-            for (int i = 1; i < count; i++) {
-                Integer key = Integer.parseInt(arrStr[i]);
-                Integer value = 0;
-                if (map.containsKey(key)) {
-                    value = map.get(key);
-                    value++;
-                }
-                map.put(key, value);
+            int[][] arrValue = new int[10001][2];
+            for (int i = 1; i < arrStr.length; i++) {
+                int index = Integer.parseInt(arrStr[i]);
+                arrValue[index][0]++;
+                arrValue[index][1] = i;
             }
-            boolean hasWin = false;
-            Iterator it = map.entrySet().iterator();
-            while (it.hasNext()) {
-                Map.Entry i = (Map.Entry) it.next();
-                int key = (int) i.getKey();
-                int value = (int) i.getValue();
-                if (value == 0) {
-                    hasWin = true;
-                    System.out.println(key);
-                    break;
+            int minSeq = Integer.MAX_VALUE;
+            int first = -1;
+            for (int i = 1; i < arrValue.length; i++) {
+                if (arrValue[i][0] == 1) {
+                    if (arrValue[i][1] < minSeq) {
+                        minSeq = arrValue[i][1];
+                        first = i;
+                    }
                 }
             }
-            if(!hasWin) System.out.println("None");
-
+            if (first!=-1) {
+                System.out.println(first);
+            }else{
+                System.out.println("None");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
 }
