d704960a4b023697adc0d03690e4698bea9237a9
VincentLaf <zsjperiod@foxmail.com>
2018-02-08 16:21:42 +0800

    1007

--- src/PAT/$1077.java
+++ src/PAT/$1077.java
@@ -1,8 +1,10 @@
+package PAT;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class $1077 {
 
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
