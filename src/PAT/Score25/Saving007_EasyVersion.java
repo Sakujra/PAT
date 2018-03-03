566408c90d84c4d9e6dba61f3c15ba3633486741
VincentLaf <zsjperiod@foxmail.com>
2018-03-03 20:57:08 +0800

    拯救007 easy version

--- src/PAT/Score25/Saving007_EasyVersion.java
+++ src/PAT/Score25/Saving007_EasyVersion.java
@@ -3,15 +3,16 @@ package PAT.Score25;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.BufferOverflowException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Saving007_EasyVersion {
 
-    private class GNode {
+    private static class GNode {
         int x;
         int y;
         boolean visited = false;
-        boolean adjcentToBank = false;
+        boolean toBank = false;
     }
 
     public static void main(String[] args) {
@@ -24,19 +25,49 @@ public class Saving007_EasyVersion {
             GNode[] nodes = new GNode[N];
             for (int i = 0; i < N; i++) {
                 strings = reader.readLine().split(" ");
-                nodes[i].x = Integer.parseInt(strings[0]);
-                nodes[i].y = Integer.parseInt(strings[1]);
-                if (50 - abs(nodes[i].x) <= D) nodes[i].adjcentToBank = true;
-                if (50 - abs(nodes[i].y) <= D) nodes[i].adjcentToBank = true;
+                GNode newNode = new GNode();
+                newNode.x = Integer.parseInt(strings[0]);
+                newNode.x = Integer.parseInt(strings[1]);
+                if (50 - abs(newNode.x) <= D) newNode.toBank = true;
+                if (50 - abs(newNode.y) <= D) newNode.toBank = true;
+                nodes[i] = newNode;
             }
 
-
+            Queue<GNode> queue = new LinkedList<>();
+            //first jump
+            for (int i = 0; i < N; i++) {
+                int x = nodes[i].x;
+                int y = nodes[i].y;
+                if (x * x + y * y <= (15 + D) * (15 + D)) {
+                    nodes[i].visited = true;
+                    queue.add(nodes[i]);
+                }
+            }
+            boolean canJumpOut = false;
+            while (!queue.isEmpty()) {
+                GNode node = queue.poll();
+                if (node.toBank) {
+                    canJumpOut = true;
+                    break;
+                }
+                for (int i = 0; i < N && !nodes[i].visited; i++) {
+                    if (pow(node.x - nodes[i].x) + pow(node.y - nodes[i].y) <= D) {
+                        nodes[i].visited = true;
+                        queue.add(nodes[i]);
+                    }
+                }
+            }
+            if (canJumpOut) System.out.println("yes");
+            else System.out.println("no");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
+    private static int pow(int n) {
+        return n * n;
+    }
 
     private static int abs(int n) {
         if (n < 0) return -n;
